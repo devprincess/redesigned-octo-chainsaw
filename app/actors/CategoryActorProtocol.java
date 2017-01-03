77b132214bea80f79cb5b5db3362bde647d4d868
Joana Chavez <chavezlucero88@gmail.com>
2017-01-03 23:30:53 +0100

    category protocol

--- app/actors/CategoryActorProtocol.java
+++ app/actors/CategoryActorProtocol.java
@@ -3,18 +3,20 @@ package actors;
 import java.io.Serializable;
 
 import models.Category;
+import play.mvc.Http;
+import play.mvc.Http.Context;
 
 public class CategoryActorProtocol {
 
 	public static class SayCategory implements Serializable{
 		public final Category category;
 
+		public SayCategory(Category category){
+			this.category = category;
+		}
 		public Category getCategory() {
 			return category;
 		}
 
-		public SayCategory(Category category) {
-			this.category = category;
-		}
 	}
 }
