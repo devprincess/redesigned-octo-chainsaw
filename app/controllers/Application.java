fe4ee8418055639493e2767708f66f5415e8dbf3
Joana Chavez <chavezlucero88@gmail.com>
2018-01-13 09:04:12 +0100

    testing the route

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -77,6 +77,8 @@ public class Application extends Controller{
 			///home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/
 			try{
 				//Files.deleteIfExists(Paths.get("/home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/public/images/", (picture.getFilename())));
+				System.out.println("File to path:"+file.toPath());
+
 				Files.copy(file.toPath(), Paths.get("/home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/public/images/", (picture.getFilename())));
 				Files.deleteIfExists(file.toPath());
 			}
@@ -143,6 +145,8 @@ public class Application extends Controller{
 			//This is for production:
 			///home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/
 			try{
+
+				System.out.println("File to path:"+file.toPath());
 				//Files.deleteIfExists(Paths.get("/home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/public/images/", (picture.getFilename())));
 				Files.copy(file.toPath(), Paths.get("/home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/public/images/", (picture.getFilename())));
 				Files.deleteIfExists(file.toPath());
