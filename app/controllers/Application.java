0c18372adc18cfe21a0b40718ff21b7f8b8a4920
Joana Chavez <chavezlucero88@gmail.com>
2018-01-20 11:14:21 +0100

    route to upload images

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -80,8 +80,10 @@ public class Application extends Controller{
 
 				System.out.println("File to path:"+file.toPath());
 
-				File movedFile = new File("/home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/public/images/", picture.getFilename());
+				File movedFile = new File("/home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/app/public/images/", picture.getFilename());
 				file.renameTo(movedFile);
+
+				System.out.println("File to path now:"+file.toPath());
 
 			}
 			catch(Exception e){
@@ -150,8 +152,11 @@ public class Application extends Controller{
 
 				System.out.println("File to path:"+file.toPath());
 
-				File movedFile = new File("/home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/public/images/", picture.getFilename());
+				File movedFile = new File("/home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/app/public/images/", picture.getFilename());
 				file.renameTo(movedFile);
+
+
+				System.out.println("File to path now:"+file.toPath());
 
 			}
 			catch(Exception e){
