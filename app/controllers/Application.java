7594bc2856f6262a0627e75bbc78fe8d12c576e1
Joana Chavez <chavezlucero88@gmail.com>
2018-01-21 10:19:20 +0100

    changing to assets

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -80,7 +80,7 @@ public class Application extends Controller{
 
 				System.out.println("File to path:"+file.toPath());
 
-				File movedFile = new File("/home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/target/classes/public/images/", picture.getFilename());
+				File movedFile = new File("/home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/assets/images/", picture.getFilename());
 				file.renameTo(movedFile);
 
 				System.out.println("File to path now:"+file.toPath());
@@ -152,7 +152,7 @@ public class Application extends Controller{
 
 				System.out.println("File to path:"+file.toPath());
 
-				File movedFile = new File("/home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/target/classes/public/images/", picture.getFilename());
+				File movedFile = new File("/home/bas/app_add52b3f-a560-49e4-b925-952452c1db3b/assets/images/", picture.getFilename());
 				file.renameTo(movedFile);
 
 
